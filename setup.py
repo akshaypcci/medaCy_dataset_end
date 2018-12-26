ea29adcc6ba414c4722feffd4eb11e58fb592963
Andriy Mulyar <aymulyar@vcu.edu>
2018-12-25 22:54:11 -0500

    Initial files for dataset

---
+++ setup.py
@@ -0,0 +1,57 @@
+from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCommand
+from medacy_dataset_end import __version__, __authors__
+import sys
+
+packages = find_packages()
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+class PyTest(TestCommand):
+    """
+    Custom Test Configuration Class
+    Read here for details: https://docs.pytest.org/en/latest/goodpractices.html
+    """
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.pytest_args = ""
+
+    def run_tests(self):
+        import shlex
+        # import here, cause outside the eggs aren't loaded
+        import pytest
+
+        errno = pytest.main(shlex.split(self.pytest_args))
+        sys.exit(errno)
+
+setup(
+    name='medacy_dataset_end',
+    version=__version__,
+    license='GNU GENERAL PUBLIC LICENSE',
+    description='medaCy compatible version of the Engineered Nanomedicine Database',
+    long_description=readme(),
+    packages=packages,
+    url='https://github.com/NanoNLP/medaCy_dataset_end',
+    author=__authors__,
+    author_email='contact@andriymulyar.com',
+    keywords='natural-language-processing medical-natural-language-processing machine-learning nlp-library metamap clinical-text-processing',
+    classifiers=[
+        '( Status :: 4 - Beta',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Programming Language :: Python :: 3.5',
+        'Natural Language :: English',
+        'Topic :: Text Processing :: Linguistic',
+        'Intended Audience :: Science/Research'
+    ],
+    install_requires=[
+        'medacy>=0.0.4',
+    ],
+    tests_require=["pytest"],
+    cmdclass={"pytest": PyTest},
+    include_package_data=True,
+    zip_safe=False
+
+)
