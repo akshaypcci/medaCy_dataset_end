0d261780559759de6e5b5984fdb399f635a49408
Andriy Mulyar <aymulyar@vcu.edu>
2019-01-21 14:21:04 -0500

    Dataset now follows template completely.

--- medacy_dataset_end/medacy_dataset_end.py
+++ medacy_dataset_end/medacy_dataset_end.py
@@ -8,7 +8,7 @@ def load():
     """
     Loads the Engineered Nanomedicine Database (END). This dataset is a collection of annotated
     FDA nanomedicine drug labels containing 28 unique entities.
-    Dataset citation: https://www.ncbi.nlm.nih.gov/pmc/articles/PMC5644562/
+    Dataset article: https://www.ncbi.nlm.nih.gov/pmc/articles/PMC5644562/
     :return: a medaCy Dataset object and a list of entities.
     """
 
@@ -31,6 +31,6 @@ def get_evaluation_dataset():
     """
     Leave the evaluation folder empty if no evaluation data is provided.
 
-    :return: a medaCy Dataset object containing this Dataset's designated training data.
+    :return: a medaCy Dataset object containing this Dataset's designated evaluation data.
     """
     return Dataset(resource_filename(package_name, 'data/evaluation'))
