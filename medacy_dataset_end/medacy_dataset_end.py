4d3f6b28bbc953a921c9b2a3933606fd1fd6fd35
Andriy Mulyar <aymulyar@vcu.edu>
2019-01-21 14:09:00 -0500

    Re-factored to match template

--- medacy_dataset_end/medacy_dataset_end.py
+++ medacy_dataset_end/medacy_dataset_end.py
@@ -1,18 +1,36 @@
 from pkg_resources import resource_listdir, resource_string, resource_filename
 from medacy.data import Dataset
-from medacy.tools import DataFile
 from os.path import join
+
+package_name = __name__
 
 def load():
     """
-    Loads the Engineered Nanomedicine Database (END) for testing purposes. This dataset is a collection of annotated
+    Loads the Engineered Nanomedicine Database (END). This dataset is a collection of annotated
     FDA nanomedicine drug labels containing 28 unique entities.
     Dataset citation: https://www.ncbi.nlm.nih.gov/pmc/articles/PMC5644562/
     :return: a medaCy Dataset object and a list of entities.
     """
-    package_name = "medacy_dataset_end"
 
     entities = resource_string(package_name, join('data', 'END.types')).decode('utf-8').split("\n")
 
-    dataset = Dataset(resource_filename(package_name, 'data'))
-    return dataset, entities
+    meta_data = {
+        'entities': entities,
+        'relations': None  # set to None if no relations
+    }
+
+    return get_training_dataset(), get_evaluation_dataset(), meta_data
+
+def get_training_dataset():
+    """
+    :return: a medaCy Dataset object containing this Dataset's designated training data.
+    """
+    return Dataset(resource_filename(package_name, 'data/training'))
+
+def get_evaluation_dataset():
+    """
+    Leave the evaluation folder empty if no evaluation data is provided.
+
+    :return: a medaCy Dataset object containing this Dataset's designated training data.
+    """
+    return Dataset(resource_filename(package_name, 'data/evaluation'))
