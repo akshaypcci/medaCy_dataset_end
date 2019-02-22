fa1dbd975b4f988cff99452da25d82f38763bd33
swfarnsworth <swfarnsworth@gmail.com>
2019-02-22 00:12:09 -0500

    Updated with latest version of data

--- medacy_dataset_end/__init__.py
+++ medacy_dataset_end/__init__.py
@@ -1,3 +1,3 @@
 from .medacy_dataset_end import load
-__version__ = '1.0.3'
-__authors__ = "Andriy Mulyar and Bridget T. McInnes"
+__version__ = '2.0.0'
+__authors__ = "Steele Farnsworth and Bridget T. McInnes"
